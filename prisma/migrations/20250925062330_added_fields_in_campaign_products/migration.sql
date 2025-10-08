651eaf840f2aa2ab8a84bc384f7cc87e56ab5cbb
sudarsanam <sudarsanam@barberaa.com>
2025-10-08 12:15:32 +0530

    db migrated to sql to pg

--- prisma/migrations/20250925062330_added_fields_in_campaign_products/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "PreorderCampaignProduct" ADD COLUMN "imageUrl" TEXT;
-ALTER TABLE "PreorderCampaignProduct" ADD COLUMN "price" DECIMAL;
-ALTER TABLE "PreorderCampaignProduct" ADD COLUMN "variantTitle" TEXT;
